405823236f6baed00c091e985906779521fbaa26
Fernando Contreras <fernandomorann999@gmail.com>
2022-03-22 00:09:19 -0600

    First commit

---
+++ part1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Mensaje message='Hola desde la consola'/>
+      <Mensaje message='Hola desde la consola x2'/>
+    </div>
+
+  );
+}
+
+export default App;
